13a8270c483670d386aa04eaae6345f7932e46c4
amwhite48 <alana_white@brown.edu>
2021-04-12 14:58:52 -0400

    changed cropping to happen w/ window size according to inputs

--- crop_img.py
+++ crop_img.py
@@ -2,38 +2,42 @@ import cv2
 import math
 import numpy as np
 
-aspect_ratio = 4/3
 
 def main():
     img = cv2.imread("basketballdude.png")
-    crop_img = crop_around_bounding_box(img, 0, 0, 100, 100)
+    crop_img = crop_around_bounding_box(img, 0, 0, 100, 100, 300, 300)
     cv2.imshow("cropped", crop_img)
     cv2.waitKey(0)
 
 
-def crop_around_bounding_box(to_crop, x, y, box_width, box_height):
-    max_padding = math.floor(box_height * aspect_ratio / 2)
+def crop_around_bounding_box(to_crop, x, y, box_width, box_height, output_width, output_height):
     # calculate center of box
     center_x = math.ceil(x + box_width/2)
-    # center_y = y + box_height/2
+    center_y = math.ceil(y + box_height/2)
     # calculate edges
     # height stays the same
-    start_y = y
-    end_y = y + box_height
+    start_y = math.floor(center_y - output_height / 2)
+    end_y = math.floor(center_y + output_height / 2)
 
-    half_crop_width = math.ceil(box_height * aspect_ratio / 2)
-    start_x = center_x - half_crop_width
-    end_x = center_x + half_crop_width
+    start_x = math.floor(center_x - output_width / 2)
+    end_x = math.floor(center_x + output_width / 2)
     # if around any edges, fill in w/ black
-    if start_x < 0 or end_x > to_crop.shape[1]:
-        pad_left = 0 - start_x 
-        if(pad_left < 0):
-            pad_left = 0
-        pad_right = end_x - to_crop.shape[1]
-        if(pad_right < 0):
-            pad_right = 0
-        padded = np.pad(to_crop, ((0,0),(pad_left, pad_right), (0,0)))
-        return padded[start_y:end_y, start_x + pad_left:end_x + pad_left]
+    if start_x < 0 or end_x > to_crop.shape[1] or start_y < 0 or end_y > to_crop.shape[0]:
+        pad_left_x = 0 - start_x 
+        if(pad_left_x < 0):
+            pad_left_x = 0
+        pad_right_x = end_x - to_crop.shape[1]
+        if(pad_right_x < 0):
+            pad_right_x = 0
+        pad_left_y = 0 - start_y
+        if(pad_left_y < 0):
+            pad_left_y = 0
+        pad_right_y = end_y - to_crop.shape[0]
+        if(pad_right_y < 0):
+            pad_right_y = 0
+        
+        padded = np.pad(to_crop, ((pad_left_y,pad_right_y),(pad_left_x, pad_right_x), (0,0)))
+        return padded[start_y + pad_left_y:end_y + pad_left_y, start_x + pad_left_x:end_x + pad_left_x]
 
 
 
