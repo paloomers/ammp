289001c95721c3090e94f5c363c163e49579fbc3
amwhite48 <alana_white@brown.edu>
2021-04-18 20:15:30 -0400

    clicking + opencv optical flow cropping

--- main.py
+++ main.py
@@ -85,14 +85,14 @@ def main():
     # imagePath = args.video
     casPath = "haarcascade_frontalface_default.xml"
     # faces = code.video_facial_recognition(args.video, args.cascade)
-    # INPUT_FILE_NAME = "./videos/bball-dribble.mp4"
-    INPUT_FILE_NAME = "0" # Use Integers for Camera (ex. Webcam)
+    INPUT_FILE_NAME = "./videos/bball-dribble.mp4"
+    # INPUT_FILE_NAME = "0" # Use Integers for Camera (ex. Webcam)
     OUTPUT_FILE_NAME = "output.avi"
     # Scale for size of output video relative to input video
     output_scale = 0.7
 
-    # method = "optical"
-    method = "cascade"
+    method = "optical"
+    # method = "cascade"
 
     # Check if INPUT_FILE_NAME is int
     try:
@@ -107,7 +107,7 @@ def main():
         # Plays Input Video
         optical.play_video(INPUT_FILE_NAME)
         # Processes Video, and Generates Output Video
-        optical.process_video(INPUT_FILE_NAME,OUTPUT_FILE_NAME,casPath,output_scale)
+        optical.process_video(INPUT_FILE_NAME,OUTPUT_FILE_NAME,output_scale)
         # Plays Output Video
         optical.play_video(OUTPUT_FILE_NAME)
 
