9a922c5f8b7bf0ba9052390dfcb53ea8aa72b76d
Paloma Salseda <Paloma@ps19201-mac.devices.brown.edu>
2021-04-22 19:42:45 -0400

    update on test real quick

--- gatheringCascade/test.py
+++ gatheringCascade/test.py
@@ -3,8 +3,6 @@ import sys
 import numpy as np
 import cv2
 
-
-#this is the cascade we just made. Call what you want
 airpod = cv2.CascadeClassifier('cascadecopy.xml')
 
 cap = cv2.VideoCapture(0)
@@ -13,11 +11,8 @@ while 1:
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
-    # add this
-    # image, reject levels level weights.
     airpods = airpod.detectMultiScale(gray, 1.3, 5)
     
-    # add this
     for (x,y,w,h) in airpods:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)
     cv2.imshow('img',img)
